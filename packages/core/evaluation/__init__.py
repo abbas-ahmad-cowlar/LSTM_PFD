442b0636afed7039bf09ac7a08cf8b3315dfc577
Syed Abbas Ahmad <syedabbasahmad6@gmail.com>
2026-02-13 18:53:22 +0500

    refactor: relocate utility scripts to proper packages\n\nMove evaluation utilities from scripts/utilities/ to packages/core/evaluation/:\n- cross_validation.py\n- temporal_cv.py\n- statistical_analysis.py\n- check_data_leakage.py\n\nMove mixed_precision_test.py to tests/benchmarks/.\n\nUpdate __init__.py files in both destination packages.

--- packages/core/evaluation/__init__.py
+++ packages/core/evaluation/__init__.py
@@ -1,3 +1,25 @@
 """Evaluation suite for model assessment."""
 
-__all__ = []
+from .cross_validation import CrossValidationTrainer
+from .temporal_cv import TimeSeriesSplit, BlockingTimeSeriesSplit, TemporalCrossValidator
+from .statistical_analysis import (
+    MultiSeedExperiment,
+    compute_confidence_interval,
+    paired_ttest,
+    wilcoxon_test,
+    compare_models
+)
+from .check_data_leakage import LeakageChecker
+
+__all__ = [
+    'CrossValidationTrainer',
+    'TimeSeriesSplit',
+    'BlockingTimeSeriesSplit',
+    'TemporalCrossValidator',
+    'MultiSeedExperiment',
+    'compute_confidence_interval',
+    'paired_ttest',
+    'wilcoxon_test',
+    'compare_models',
+    'LeakageChecker',
+]
